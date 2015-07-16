28ed6393a15e7efc60ab02b2bf49cad121010acd
dey2929 <deyanand2929@gmail.com>
2015-07-17 01:28:53 +0530

    -Edit friends improved !

--- app/src/main/java/com/aset/dey/peptalk/EditFriendsActivity.java
+++ app/src/main/java/com/aset/dey/peptalk/EditFriendsActivity.java
@@ -1,28 +1,74 @@
 package com.aset.dey.peptalk;
 
+import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.nfc.Tag;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.util.List;
 
-public class EditFriendsActivity extends ActionBarActivity {
+
+public class EditFriendsActivity extends ListActivity {
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
+    protected List<ParseUser> mUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_friends);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+        setProgressBarIndeterminateVisibility(true);
         ParseQuery<ParseUser> query = ParseUser.getQuery();
         query.orderByAscending(ParseConstants.KEY_USERNAME);//arranges user by ascending order
         query.setLimit(1000);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
+                if(e==null)
+                {
+                    mUsers=users;
+                    String[] usernames=new String[mUsers.size()];//creating an array to put list of users
+                    int i=0;
+                    for(ParseUser user:mUsers)
+                    {
+                        usernames[i]=user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this,android.R.layout.simple_list_item_checked,usernames);
+                    setListAdapter(adapter);
+                    //success
+                }
+                else
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+                    builder.setMessage(e.getMessage());
+                    builder.setTitle(R.string.ErrorTitle);
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    AlertDialog Dialog = builder.create();
+                    Dialog.show();
+                    Log.e(TAG,e.getMessage());//logging the error message for the developer
+                }
+            }
+        });
+
     }
 
     @Override
