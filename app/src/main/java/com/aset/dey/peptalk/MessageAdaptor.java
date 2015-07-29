f8c47ef3b6b3afb4141b7b95a52dce5dcaba35cb
dey2929 <deyanand2929@gmail.com>
2015-07-29 11:38:31 +0530

    only SelfDestruct left ! theming left

--- app/src/main/java/com/aset/dey/peptalk/MessageAdaptor.java
+++ app/src/main/java/com/aset/dey/peptalk/MessageAdaptor.java
@@ -1,5 +1,7 @@
 package com.aset.dey.peptalk;
 
+import java.util.List;
+
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,34 +12,34 @@ import android.widget.TextView;
 
 import com.parse.ParseObject;
 
-import java.util.List;
-
-/**
- * Created by dey on 29-Jul-15.
- */
 public class MessageAdaptor extends ArrayAdapter<ParseObject> {
+
     protected Context mContext;
     protected List<ParseObject> mMessages;
-    public MessageAdaptor(Context context,List<ParseObject> messages)
-    {super(context,R.layout.message_item,messages);
+
+    public MessageAdaptor(Context context, List<ParseObject> messages) {
+        super(context, R.layout.message_item, messages);
         mContext = context;
-        mMessages= messages;
+        mMessages = messages;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
-        if(convertView==null) {
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);//Layout inflater converts xml to views in android
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
             holder = new ViewHolder();
-            holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
-            holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
+            holder.iconImageView = (ImageView)convertView.findViewById(R.id.messageIcon);
+            holder.nameLabel = (TextView)convertView.findViewById(R.id.senderLabel);
         }
         else {
             holder = (ViewHolder)convertView.getTag();
         }
+
         ParseObject message = mMessages.get(position);
-        if(message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
+
+        if (message.getString(ParseConstants.KEY_FILE_TYPE).equals(ParseConstants.TYPE_IMAGE)) {
             holder.iconImageView.setImageResource(R.drawable.ic_action_photo);
         }
         else
@@ -45,12 +47,20 @@ public class MessageAdaptor extends ArrayAdapter<ParseObject> {
             holder.iconImageView.setImageResource(R.drawable.ic_action_name);
         }
         holder.nameLabel.setText(message.getString(ParseConstants.KEY_SENDER_NAME));
-            return null;
-    }
-   public static class ViewHolder//holds data to be displayed in custom layout
-   {
-       ImageView iconImageView;
-       TextView nameLabel;
+        convertView.setTag(holder);
 
-   }
+        return convertView;
+    }
+
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView nameLabel;
+    }
 }
+
+
+
+
+
+
---
+++ app/src/main/java/com/aset/dey/peptalk/ViewImageActivity.java
@@ -0,0 +1,43 @@
+package com.aset.dey.peptalk;
+
+import android.net.Uri;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+
+public class ViewImageActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_image);
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        Uri imageUri = getIntent().getData();
+        Picasso.with(this).load(imageUri.toString()).into(imageView);//picasso loads the image from the url online to the imageview we created
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // This ID represents the Home or Up button. In the case of this
+                // activity, the Up button is shown. Use NavUtils to allow users
+                // to navigate up one level in the application structure. For
+                // more details, see the Navigation pattern on Android Design:
+                //
+                // http://developer.android.com/design/patterns/navigation.html#up-vs-back
+                //
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
